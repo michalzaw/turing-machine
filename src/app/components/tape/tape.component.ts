bfc9aca6e8eb46606c9ae992b3d56418f81b54ae
michalzaw <mich_z@op.pl>
2019-06-12 20:13:30 +0200

    Moved 'next step' button to app component.

--- src/app/components/tape/tape.component.ts
+++ src/app/components/tape/tape.component.ts
@@ -14,34 +14,10 @@ export class TapeComponent implements OnInit {
 
   @Input() currentCell: number;
   @Input() currentState: number;
-  
-  @Output() onStateChanged: EventEmitter<number> = new EventEmitter<number>();
-  @Output() onCellChanged: EventEmitter<number> = new EventEmitter<number>();
 
   constructor() { }
 
   ngOnInit() {
-  }
-
-  next() {
-    let symbolInCurrentTapeCell = this.tape[this.currentCell];
-    let currentCellInStateTable = this.states[this.currentState][symbolInCurrentTapeCell];
-
-    this.tape[this.currentCell] = currentCellInStateTable.symbol;
-    this.currentCell += currentCellInStateTable.direction;
-    this.currentState = currentCellInStateTable.nextState;
-
-    if (this.currentCell > this.tape.length - 3) {
-      this.tape.push(0);
-    }
-
-    if (this.currentCell < 3) {
-      this.tape.unshift(0);
-      this.currentCell++;
-    }
-
-    this.onStateChanged.emit(this.currentState);
-    this.onCellChanged.emit(this.currentCell);
   }
 
 }
